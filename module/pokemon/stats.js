4c64fff04c6ab10c787333472b66b0151a7d35f0
Skario <kevin.courriels@gmail.com>
2020-06-15 23:10:47 +0200

    Add nature support in stats calculation

--- module/pokemon/stats.js
+++ module/pokemon/stats.js
@@ -1,10 +1,20 @@
 module.exports = (client) => {
     client.pokemon.stats = {};
-    client.pokemon.stats.calcHP = ( level, iv, ev, base ) => {
+    const calcHP = ( level, iv, ev, base ) => {
         return Math.floor( ( ( ( 2 * base + iv + ev / 4 ) * level ) / 100 ) + level + 10 );
     };
 
-    client.pokemon.stats.calcStat = ( level, iv, ev, base, nature ) => {
-        return Math.floor( ( ( ( 2 * base + iv + ev / 4 ) * level ) / 100 + 5 ) * nature );
+    client.pokemon.stats.calcStat = ( nameStat, level, iv, ev, base, nature = {} ) => {
+        if(nameStat === "hp ") {
+            return calcHP( level, iv, ev, base );
+        } else {
+            let mult = 1.0;
+            if(typeof nature.decreased_stat !== "undefined" && nature.decreased_stat !== null) {
+                if(nature.decreased_stat.name === nameStat) mult = 0.9;
+            } else if(typeof nature.increased_stat !== "undefined" && nature.increased_stat !== null) {
+                if(nature.increased_stat === nameStat) mult = 1.1;
+            }
+            return Math.floor( ( ( ( 2 * base + iv + ev / 4 ) * level ) / 100 + 5 ) * mult );
+        }
     };
 };
