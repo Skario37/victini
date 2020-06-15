780a1c352c92ae716c533a1bc56b20316122a7cc
Kévin <kevin.courriels@gmail.com>
2020-06-15 13:44:26 +0200

    Add getters for nature, growth-rate, moves and item

--- module/pokemon/services/read.js
+++ module/pokemon/services/read.js
@@ -3,31 +3,80 @@ const options = { encoding: 'utf8' };
 const assets_path = "./../../../assets/";
 const pokemon_path_from_assets = "json/pokemon/";
 const gender_path_from_assets = "json/gender/";
+const nature_path_from_assets = "json/nature/";
+const growth_path_from_assets = "json/growth-rate/";
+const move_path_from_assets = "json/moves/";
+const item_path_from_assets = "json/item/";
 const json_ext = ".json";
 
 exports.getPokemonByID = (client, _id) => {
-  return fs.readFileSync(
+  return JSON.parse(fs.readFileSync(
     `${assets_path}${pokemon_path_from_assets}${client.addBeginZero(_id, 4)}${json_ext}`, 
     options
-  );
+  ));
 };
 exports.getPokemonVarietyByID = (client, _id, n = 0) => {
-  return fs.readFileSync(
+  return JSON.parse(fs.readFileSync(
     `${assets_path}${pokemon_path_from_assets}${client.addBeginZero(_id, 4)}varieties/${client.addBeginZero(n, 3)}${json_ext}`, 
     options
-  );
+  ));
 };
 exports.getPokemonFormByID = (client, _id, n = 0) => {
-  return fs.readFileSync(
+  return JSON.parse(fs.readFileSync(
     `${assets_path}${pokemon_path_from_assets}${client.addBeginZero(_id, 4)}forms/${client.addBeginZero(n, 3)}${json_ext}`, 
     options
-  );
+  ));
 };
 
 
-exports.getGenderByID = (client, id = 0) => {
-  return fs.readFileSync(
+exports.getGenderByID = (client, _id = 3) => {
+  return JSON.parse(fs.readFileSync(
     `${assets_path}${gender_path_from_assets}${client.addBeginZero(_id, 4)}${json_ext}`, 
     options
-  );
+  ));
+}
+
+exports.getNaturesLength = () => {
+  return fs.readdir(
+    `${assets_path}${nature_path_from_assets}`, 
+    (err, files) => {
+    return files.length;
+  });
+}
+exports.getNatureByID = (client, _id = 1) => {
+  return JSON.parse(fs.readFileSync(
+    `${assets_path}${nature_path_from_assets}${client.addBeginZero(_id, 4)}${json_ext}`, 
+    options
+  ));
+}
+
+exports.getGrowthLength = () => {
+  return fs.readdir(
+    `${assets_path}${growth_path_from_assets}`, 
+    (err, files) => {
+    return files.length;
+  });
+}
+exports.getGrowthByURL = (url) => {
+  return JSON.parse(fs.readFileSync(
+    `${assets_path}${url}`, 
+    options
+  ));
+}
+
+exports.getMoveByID = (client, _id) => {
+  return JSON.parse(fs.readFileSync(
+    `${assets_path}${move_path_from_assets}${client.addBeginZero(_id, 4)}${json_ext}`, 
+    options
+  ));
+}
+exports.getMoveUrlByID = (client, _id) => {
+  return `${move_path_from_assets}${client.addBeginZero(_id, 4)}${json_ext}`;
+}
+
+exports.getItemByID = (client, _id) => {
+  return JSON.parse(fs.readFileSync(
+    `${assets_path}${item_path_from_assets}${client.addBeginZero(_id, 4)}${json_ext}`, 
+    options
+  ));
 }
