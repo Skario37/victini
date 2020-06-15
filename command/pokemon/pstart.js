1e4e402ba6ca479ef6d4c88f74a0d825c8651834
Kévin <kevin.courriels@gmail.com>
2020-06-15 13:45:20 +0200

    Add encountered_location property

--- command/pokemon/pstart.js
+++ command/pokemon/pstart.js
@@ -25,11 +25,11 @@ exports.run = async (client, message, args, level) => {
     if ( temp.length > 0 ) {
         starters = temp.pokemon;
     } else {
-        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "1", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true } ) );
-        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "4", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true } ) );
-        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "7", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true } ) );
-        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "25", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true } ) );
-        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "133", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true } ) );
+        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "1", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true, "encountered_location" : message.channel } ) );
+        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "4", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true, "encountered_location" : message.channel } ) );
+        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "7", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true, "encountered_location" : message.channel } ) );
+        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "25", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true, "encountered_location" : message.channel } ) );
+        starters.push( await client.pokemon.generatePokemon( client, { "_id" : "133", "level" : 5, "isShinyLock" : true, "iv" : 3, "origin_trainer" : { "name" : message.author.username, "id" : trainer.trainerID }, "isStarter" : true, "encountered_location" : message.channel } ) );
         await client.database.db_trainer.request.setStarter( client, message.member, starters );
     }
 
