dd9704015578d79ad6711948c7a81e5165b76804
Skario <kevin.courriels@gmail.com>
2020-06-21 16:23:41 +0200

    Add check if db exists

--- event/guildCreate.js
+++ event/guildCreate.js
@@ -3,5 +3,8 @@
 module.exports = (client, guild) => {
   client.logger.cmd(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`);
   if(guild.id != client.config.guildID && guild.id != client.config.guildDevID) guild.leave(); // Leave guild
-  client.database.db_spawn.request.init(client, guild.id);
+  
+  if(!client.database.db_spawn.request.exists(client, guild.id)) {
+    client.database.db_spawn.request.init(client, guild.id);
+  }
 };
